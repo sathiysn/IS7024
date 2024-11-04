1799883ca854ae1ac3d090eb127108e7d3fa3402
Rhiannon Wallace <wallacr7@mail.uc.edu>
2024-11-04 13:48:38 -0500

    Created User class

--- HealthWatch360/Models/User.cs
+++ HealthWatch360/Models/User.cs
@@ -1,8 +1,17 @@
-﻿using System;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
-public class User
+namespace HealthWatch360.Models
 {
-	public User()
+	public class User
 	{
+		public string UserID { get; set; }
+		public string? FirstName { get; set; }
+		public string? LastName { get; set; }
+		public string? Email { get; set; }
+		//public DateTime? DOB { get; set; }
+		public decimal? Weight { get; set; }
+		public decimal? Height { get; set; }
+		public string? Gender { get; set; }
 	}
 }
