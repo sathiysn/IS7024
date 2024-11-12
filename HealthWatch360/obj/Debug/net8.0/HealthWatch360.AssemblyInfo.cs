7b59fc61601446c6e37844dd32c700a12718b724
Bengeo Benny George <95736149+bengeobenny@users.noreply.github.com>
2024-11-12 14:17:27 -0500

    Added sliding health tips

--- HealthWatch360/obj/Debug/net8.0/HealthWatch360.AssemblyInfo.cs
+++ HealthWatch360/obj/Debug/net8.0/HealthWatch360.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthWatch360")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62229d8e349a027685e6516c006f3eb78231e3d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73a18572ae09925a7597c5fd868686c4a76dc48e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthWatch360")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthWatch360")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
