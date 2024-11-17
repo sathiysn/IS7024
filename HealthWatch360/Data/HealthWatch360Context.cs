192abc791386514b2f308bc9b2e229df2afe5aeb
Rhiannon Wallace <wallacr7@mail.uc.edu>
2024-11-17 11:55:02 -0500

    Working to get most updated branch + gitignore

---
+++ HealthWatch360/Data/HealthWatch360Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HealthWatch360.Models;
+
+namespace HealthWatch360.Data
+{
+    public class HealthWatch360Context : DbContext
+    {
+        public HealthWatch360Context (DbContextOptions<HealthWatch360Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HealthWatch360.Models.HealthReport> HealthReport { get; set; } = default!;
+        public DbSet<HealthWatch360.Models.User> User { get; set; } = default!;
+    }
+}
---
+++ HealthWatch360/Models/Goal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+//Purpose: tracks and manages a user's personal health goals
+//Uses: allows a user to set health-related goals, track their progress, and provides updates and reminders to motivate a user
+
+namespace HealthWatch360.Models
+{
+    public class Goal
+    {
+        public int GoalID { get; set; }
+
+        public string? Type { get; set; }
+
+        public string? Description { get; set; }
+
+        [DisplayName("Target Value")]
+        public int? TargetValue { get; set; }
+
+        [DisplayName("Current Value")]
+        public int? CurrentValue { get; set; }
+
+        [DisplayName("Date of Workout")]
+        [DataType(DataType.Date)]
+        public DateTime? StartDate { get; set; }
+
+
+        //Goal <-> User Relationship
+        public User? User { get; set; }
+        public int? UserID { get; set; }
+
+        ////Goal <-> NutritionLog Relationship
+        //public List<NutritionLog>? NutritionLog { get; set; }
+
+        ////Goal <-> ExerciseLog Relationship
+        //public List<ExerciseLog>? ExerciseLog { get; set; }
+    }
+}
