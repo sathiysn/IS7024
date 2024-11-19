604720c66ba16a85f083dda7d0711e305ffe5530
Sahana Sathiyanarayanan <sathiysn@mail.uc.edu>
2024-11-19 10:30:02 -0500

    Created Nutrition Card in reports page

--- HealthWatch360/Pages/Reports.cshtml.cs
+++ HealthWatch360/Pages/Reports.cshtml.cs
@@ -1,12 +1,20 @@
+using HealthWatch360.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using static HealthWatch360.Models.NutritionLog;
 
 namespace HealthWatch360.Pages
 {
     public class ReportsModel : PageModel
     {
+        public NutritionLog.Root FoundationFoods { get; set; }
+        public Root? Root { get; private set; }
+
         public void OnGet()
         {
+            var jsonData = System.IO.File.ReadAllText("NutritionInfo.json");
+            Root = JsonConvert.DeserializeObject<NutritionLog.Root>(jsonData);
         }
     }
 }
