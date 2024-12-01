2a0f78f2f63c823f934fc854fa161e9515174316
Bengeo Benny George <95736149+bengeobenny@users.noreply.github.com>
2024-12-01 09:26:17 -0500

    Added model and classes

---
+++ HealthWatch360/Models/VideoContent.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Mindlift.Models
+{
+    public class VideoContent
+    {
+        public int VideoId { get; set; }
+        public string VideoTitle { get; set; }
+        public string Url { get; set; }
+        public TimeSpan Duration { get; set; }
+        public string Tags { get; set; }
+    }
+}
