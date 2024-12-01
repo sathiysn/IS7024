c676edf0a9f11040f75ee2029064d45d8580cccf
Megha0325 <69755581+Megha0325@users.noreply.github.com>
2024-12-01 00:41:04 -0500

    Integrate other teams API to our website

---
+++ HealthWatch360/Models/Library.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json.Converters;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace HealthWatch360.Models
+{
+    public partial class Library
+    {
+        public class Book
+        {
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public string ISBN { get; set; }
+            public string PublishYear { get; set; }
+        }
+    }
+}
